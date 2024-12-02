5d1b52c50c7c43a6198c74d4ec79beff8be9e497
Yongmin Kwon <46968697+yongmin21@users.noreply.github.com>
2024-12-02 23:47:39 +0900

    Delete src directory

--- src/metric.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def NMAE(true, pred, capacity=20700):
-    mae = np.mean(np.abs(true - pred))
-    score = mae / capacity #np.mean(np.abs(true)) - original nmae
-    return score * 100
